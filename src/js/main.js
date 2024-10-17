de5197656fb04f517ffeea6f145acdedb0846a26
Zhandos <zh.zhanybek@gmail.com>
2024-10-17 23:44:39 +0500

    feat: написать js скрипт для функционала открывающего меню в шапке

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
-;(() => {
+(() => {
+  window.addEventListener("load", function () {
+    feather.replace();
+    const menuButton = document.querySelector("#menuButton");
+    const mobileMenu = document.querySelector(".header-mobile-menu");
+    let isMobileMenuOpened = false;
 
+    function toggleMobileMenu(state) {
+      isMobileMenuOpened = state !== undefined ? state : !isMobileMenuOpened;
+      menuButton.classList.toggle("_active", isMobileMenuOpened);
+      mobileMenu.classList.toggle("_active", isMobileMenuOpened);
+    }
+
+    if (menuButton && mobileMenu) {
+      menuButton.addEventListener("click", (e) => {
+        e.stopPropagation();
+        toggleMobileMenu();
+      });
+    }
+    document.body.addEventListener("click", (e) => {
+      if (!mobileMenu.contains(e.target) && e.target !== menuButton) {
+        toggleMobileMenu(false);
+      }
+    });
+  });
 })();
